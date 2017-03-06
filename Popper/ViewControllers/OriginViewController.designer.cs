1024d586b58be79b35d6df0187bc84071d16b004
Juho Kaasalainen <juho@taiste.fi>
2017-03-06 21:17:05 +0200

    Initial commit.

---
+++ Popper/ViewControllers/OriginViewController.designer.cs
@@ -0,0 +1,58 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Popper
+{
+	[Register ("OriginViewController")]
+	partial class OriginViewController
+	{
+		[Outlet]
+		UIKit.UIButton Button1 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Button2 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Button3 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Button4 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Button5 { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (Button1 != null) {
+				Button1.Dispose ();
+				Button1 = null;
+			}
+
+			if (Button2 != null) {
+				Button2.Dispose ();
+				Button2 = null;
+			}
+
+			if (Button3 != null) {
+				Button3.Dispose ();
+				Button3 = null;
+			}
+
+			if (Button4 != null) {
+				Button4.Dispose ();
+				Button4 = null;
+			}
+
+			if (Button5 != null) {
+				Button5.Dispose ();
+				Button5 = null;
+			}
+		}
+	}
+}
